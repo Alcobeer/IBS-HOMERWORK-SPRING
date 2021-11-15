590cbdc0330e63895025af7ac9c34485c218768d
Mat_pas2xov@mail.ru <Civilizacia2560>
2021-11-15 17:00:52 +0300

    Реализовал калькулятор

--- src/main/java/ru/com/ibs/homerwork3/controller/HelloController.java
+++ src/main/java/ru/com/ibs/homerwork3/controller/HelloController.java
@@ -1,15 +1,34 @@
 package ru.com.ibs.homerwork3.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 //@RequestMapping("/my")
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b",required = false)int b,
+                           @RequestParam(value="doing",required = false)String doing,
+                           Model model){
+
+       // System.out.println("Hello "+name+" "+surname);
+
+        switch (doing){
+            case "add":model.addAttribute("doing",a+b);
+            break;
+            case "sub":model.addAttribute("doing",a-b);
+            break;
+            case "mul":model.addAttribute("doing",a*b);
+            break;
+            case "div":model.addAttribute("doing",a/(double)b);
+            break;
+
+        }
+        //model.addAttribute("message", "Hello "+name+" "+surname);
         return "hello";
     }
 }
